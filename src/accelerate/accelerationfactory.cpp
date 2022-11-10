00889a54e2789dd900790e0f5a4ce945a6c93da0
Florian Frohn <ffrohn@batman>
2022-11-10 15:42:59 +0100

    disable acceleration via QE for now

--- src/accelerate/accelerationfactory.cpp
+++ src/accelerate/accelerationfactory.cpp
@@ -5,9 +5,9 @@
 using AT = std::unique_ptr<AccelerationTechnique<IntTheory, BoolTheory>>;
 
 AT AccelerationFactory::get(const LinearRule &rule, option<Recurrence::Result> closed, ITSProblem &its) {
-    if (closed && rule.getGuard()->isPoly() && closed->update.isPoly()) {
-        return AT(new AccelerationViaQE(rule, *closed, its));
-    } else {
+//    if (closed && rule.getGuard()->isPoly() && closed->update.isPoly()) {
+//        return AT(new AccelerationViaQE(rule, *closed, its));
+//    } else {
         return AT(new AccelerationViaCalculus(rule, closed, its));
-    }
+//    }
 }
