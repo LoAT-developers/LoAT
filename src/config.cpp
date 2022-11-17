dba7d2d73dbaa197067fc86fd5817c257fb5d3d1
Florian Frohn <ffrohn@batman>
2022-11-17 14:07:01 +0100

    try larger SMT timeout

--- src/config.cpp
+++ src/config.cpp
@@ -63,7 +63,7 @@ namespace Config {
 
     namespace Smt {
         // Timeouts (default / for metering / for limit smt encoding)
-        const unsigned DefaultTimeout = 500u;
+        const unsigned DefaultTimeout = 2000u;
         const unsigned MeterTimeout = 500u;
         const unsigned StrengtheningTimeout = 750u;
         const unsigned LimitTimeout = 500u;
