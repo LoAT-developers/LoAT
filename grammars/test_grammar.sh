e67a08c18fe83cf8578b96d79f39e6f489f3adac
Florian Frohn <ffrohn@batman>
2022-12-07 13:27:12 +0100

    fix script for testing grammars

--- grammars/test_grammar.sh
+++ grammars/test_grammar.sh
@@ -1,3 +1,3 @@
 # usage: test_grammar.sh $GRAMMAR_NAME $START_RULE $TEST_FILE
 javac -cp ../deps/antlr-4.11.1-complete.jar:$CLASSPATH *.java
-/usr/share/antlr4/grun $@
+java -cp ../deps/antlr-4.11.1-complete.jar:$CLASSPATH org.antlr.v4.gui.TestRig $@
