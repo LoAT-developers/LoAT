fd197c865c555e89daa0e79e4b132db303ef9cbf
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2023-07-13 13:27:39 +0200

    fix output for version

--- src/main.cpp
+++ src/main.cpp
@@ -174,7 +174,7 @@ int main(int argc, char *argv[]) {
     }
     yices::exit();
 
-    cout << "Build SHA: " << Version::GIT_SHA << (Version::GIT_DIRTY == "DIRTY" ? " (dirty)" : "") << endl;
+    cout << "Build SHA: " << Version::GIT_SHA << " (" << Version::GIT_DIRTY << ")" << endl;
 
     return 0;
 }
