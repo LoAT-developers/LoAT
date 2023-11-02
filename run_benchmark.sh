aaecfae5e486d2a46a21fa66d4d991b28b32b21c
Niklas Gruhn <niklas@gruhn.me>
2023-11-02 12:03:32 +0100

    non-linear: same file for benchmark results of Z3/ADCL
    put benchmark results for Z3 and ADCL in the same file so they can
    be compared during a test run more easily.

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -12,14 +12,24 @@ pushd build
 make -j4
 popd
 
-# # debug: 009
+
+# 074
+# 106   
+# 109   
+# 110   
+# 111   
+# 113   
+# 114 
+
+# TODO: debug 342 (seg fault)
+
 # # gdb --args \
 # ./build/loat-static \
 #   --mode reachability \
 #   --format horn \
 #   --proof-level 0 \
 #   --log \
-#   "../chc-comp22-benchmarks/LIA/chc-LIA_265.smt2"
+#   "../chc-comp22-benchmarks/LIA/chc-LIA_074.smt2"
 
 # popd
 # exit
@@ -27,19 +37,20 @@ popd
 ##########################################################################
 
 benchmark="LIA"
-compare_with="adcl"
+
+printf "### %-7s %-7s %-7s \n" "Z3" "ADCL" "now"
 
 while IFS= read -r line
 do
-  if [[ $line =~ ^#.*$ ]] || [ "$line" = "" ] ; then
+  if [[ $line =~ ^#.*$ ]] || [[ "$line" = "" ]]; then
     # skip comments and empty lines
     continue
   else
-    read idx prev_result <<< "$line"
+    read idx z3_result adcl_result <<< "$line"
     file="../chc-comp22-benchmarks/${benchmark}/chc-${benchmark}_${idx}.smt2"
 
     # if true; then
-    if [[ "$prev_result" != "timeout" ]]; then
+    if [[ "$z3_result" != "sat" ]] && [[ "$z3_result" != "timeout" ]]; then
       set +e
       result=$(timeout 5 ./build/loat-static --mode reachability --format horn --proof-level 0 "$file")
       # result=$(timeout 5 z3 "$file")
@@ -55,19 +66,16 @@ do
         result="error"
       fi
 
-      if [[ "$result" == "$prev_result" ]]; then
-        printf "$idx $result \n"
-      else
-        printf "$idx $prev_result --> $result \n"
-      fi
-    else
-      # if we skip an instance nevertheless include it in the output
-      # so the log can easily be copied and saved as a whole
-      printf "$idx $prev_result \n"
+      printf "$idx %-7s %-7s %-7s \n" $z3_result $adcl_result $result
+
+    # else
+    #   # if we skip an instance nevertheless include it in the output
+    #   # so the log can easily be copied and saved as a whole
+    #   printf "$idx $prev_result \n"
     fi
 
   fi
-done < "./benchmarks/${benchmark}_${compare_with}.txt"
+done < "./benchmarks/${benchmark}.txt"
 
 # "undo" pushd
 popd
