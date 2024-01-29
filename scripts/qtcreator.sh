f1c94ab8ddb143adc5292b97a261135c380096ad
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2024-01-29 17:50:06 +0100

    improve scripting

--- scripts/qtcreator.sh
+++ scripts/qtcreator.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker compose -f ../docker/docker-compose.yml build --build-arg TOKEN="$(xauth list)"
-docker compose -f ../docker/docker-compose.yml up
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+docker compose -f $SCRIPT_DIR/../docker/docker-compose.yml build --build-arg TOKEN="$(xauth list)"
+docker compose -f $SCRIPT_DIR/../docker/docker-compose.yml up
