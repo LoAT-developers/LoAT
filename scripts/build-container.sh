1269707b5383dcf2686cf1e98d2c42384ac5961b
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2024-03-05 13:54:20 +0100

    build bundle inside docker

--- scripts/build-container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Triggers the build of a docker container with all the dependencies that are
-# needed to compile LoAT.
-
-docker build --target loat-docker --no-cache-filter loat-docker -t loat-docker ..
