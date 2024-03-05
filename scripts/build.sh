a85e906f5cd4229e4a704db1f7c0edacd8dd7077
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2024-03-05 13:54:20 +0100

    build bundle inside docker

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-docker compose --file $SCRIPT_DIR/../docker/docker-compose.build.yml build
-docker compose --file $SCRIPT_DIR/../docker/docker-compose.build.yml up
