6c04a597645e5f28e720bd4cb8cde06b9bbe0da5
github-action-benchmark <github@users.noreply.github.com>
2024-03-08 07:45:12 +0000

    add LoAT Benchmarks (customBiggerIsBetter) benchmark result for 4da83e4b770b9039866fdd9db137ef08c2c49eba

---
+++ docs/dev/bench/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1709883912498,
+  "repoUrl": "https://github.com/LoAT-developers/LoAT",
+  "entries": {
+    "LoAT Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "Florian Frohn",
+            "username": "ffrohn",
+            "email": "florian.frohn@cs.rwth-aachen.de"
+          },
+          "committer": {
+            "name": "Florian Frohn",
+            "username": "ffrohn",
+            "email": "florian.frohn@cs.rwth-aachen.de"
+          },
+          "id": "4da83e4b770b9039866fdd9db137ef08c2c49eba",
+          "message": "checkout",
+          "timestamp": "2024-03-08T07:39:43Z",
+          "url": "https://github.com/LoAT-developers/LoAT/commit/4da83e4b770b9039866fdd9db137ef08c2c49eba"
+        },
+        "date": 1709883909103,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "chc-comp22-benchmarks unknown",
+            "value": "157",
+            "unit": "number"
+          },
+          {
+            "name": "chc-comp22-benchmarks unsat",
+            "value": "85",
+            "unit": "number"
+          },
+          {
+            "name": "chc-comp22-benchmarks timeout",
+            "value": "257",
+            "unit": "number"
+          },
+          {
+            "name": "chc-comp23-benchmarks unknown",
+            "value": "125",
+            "unit": "number"
+          },
+          {
+            "name": "chc-comp23-benchmarks unsat",
+            "value": "52",
+            "unit": "number"
+          },
+          {
+            "name": "chc-comp23-benchmarks timeout",
+            "value": "245",
+            "unit": "number"
+          }
+        ]
+      }
+    ]
+  }
+}
