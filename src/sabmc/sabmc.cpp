24227c3de41a7cd958529fe755adc6645c6725f4
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2024-05-17 22:29:06 +0200

    Revert "blocking clauses for original clauses"
    This reverts commit 50b22931a64b04404f03698c73ccaf014b598804.

--- src/sabmc/sabmc.cpp
+++ src/sabmc/sabmc.cpp
@@ -473,12 +473,13 @@ Bools::Expr SABMC::encode_transition(const Bools::Expr &t) {
 void SABMC::add_blocking_clauses() {
     // std::cout << "BLOCKING CLAUSES" << std::endl;
     for (auto from = 0; from <= depth; ++from) {
-        for (const auto &[id,trans] : rule_map) {
-            for (auto to = id <= last_orig_clause ? from + 2 : from + 1; to <= depth + 1; ++to) {
+        for (const auto &b : blocked) {
+            const auto id {arith::mkConst(rule_map.right.at(b.trans))};
+            for (auto to = from + 1; to <= depth + 1; ++to) {
                 const auto s {get_subs(from, to - from)};
-                auto block {s(!trans)};
+                auto block {s(!b.trans)};
                 if (to == from + 1) {
-                    block = block || bools::mkLit(arith::mkGeq(s.get<Arith>(trace_var), arith::mkConst(id)));
+                    block = block || bools::mkLit(arith::mkGeq(s.get<Arith>(trace_var), id));
                 }
                 solver->add(block);
                     // std::cout << "trans: " << b.trans << std::endl;
