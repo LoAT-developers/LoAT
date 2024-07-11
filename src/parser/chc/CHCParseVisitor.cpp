4a44115e7ba992b2c5b22029c0d5ee9720cb3d2b
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2024-07-11 10:52:14 +0200

    split disjunctive clauses

--- src/parser/chc/CHCParseVisitor.cpp
+++ src/parser/chc/CHCParseVisitor.cpp
@@ -60,60 +60,63 @@ antlrcpp::Any CHCParseVisitor::visitMain(CHCParser::MainContext *ctx) {
         bvars.emplace_back(BoolVar::nextProgVar());
     }
     for (const Clause &c: clauses) {
-        Subs ren;
-        // replace the arguments of the body predicate with the corresponding program variables
-        unsigned bool_arg {0};
-        unsigned int_arg {0};
-        for (const auto &x: c.lhs.args) {
-            std::visit(
-                Overload{[&](const Arith::Var x) {
-                             ren.put<Arith>(x, vars[int_arg]);
-                             ++int_arg;
-                         },
-                         [&](const Bools::Var x) {
-                             ren.put<Bools>(x, bools::mkLit(bools::mk(bvars[bool_arg])));
-                             ++bool_arg;
-                         }}
-                , x);
-        }
-        VarSet cVars;
-        for (const auto &var: c.rhs.args) {
-            cVars.insert(var);
-        }
-        c.guard->collectVars(cVars);
-        // replace all other variables from the clause with temporary variables
-        for (const auto &x: cVars) {
-            if (!ren.contains(x)) {
+        const auto disjuncts = c.guard->isOr() ? c.guard->getChildren() : BoolExprSet{c.guard};
+        for (const auto &g: disjuncts) {
+            Subs ren;
+            // replace the arguments of the body predicate with the corresponding program variables
+            unsigned bool_arg {0};
+            unsigned int_arg {0};
+            for (const auto &x: c.lhs.args) {
                 std::visit(
-                    Overload{[&](const auto var) {
-                        const auto th {theory::theory(var)};
-                        ren.put<decltype(th)>(var, th.varToExpr(var->next()));
-                    }}, x);
+                    Overload{[&](const Arith::Var x) {
+                                ren.put<Arith>(x, vars[int_arg]);
+                                ++int_arg;
+                            },
+                            [&](const Bools::Var x) {
+                                ren.put<Bools>(x, bools::mkLit(bools::mk(bvars[bool_arg])));
+                                ++bool_arg;
+                            }}
+                    , x);
             }
+            VarSet cVars;
+            for (const auto &var: c.rhs.args) {
+                cVars.insert(var);
+            }
+            g->collectVars(cVars);
+            // replace all other variables from the clause with temporary variables
+            for (const auto &x: cVars) {
+                if (!ren.contains(x)) {
+                    std::visit(
+                        Overload{[&](const auto var) {
+                            const auto th {theory::theory(var)};
+                            ren.put<decltype(th)>(var, th.varToExpr(var->next()));
+                        }}, x);
+                }
+            }
+            bool_arg = 0;
+            int_arg = 0;
+            Subs up;
+            for (const auto &arg: c.rhs.args) {
+                std::visit(
+                    Overload{[&](const Arith::Var var) {
+                                up.put<Arith>(vars[int_arg], ren.get<Arith>(var));
+                                ++int_arg;
+                            },
+                            [&](const Bools::Var var) {
+                                up.put<Bools>(bvars[bool_arg], ren.get<Bools>(var));
+                                ++bool_arg;
+                            }}, arg);
+            }
+            for (unsigned i = int_arg; i < max_int_arity; ++i) {
+                up.put<Arith>(vars[i], ArithVar::next()->toExpr());
+            }
+            for (unsigned i = bool_arg; i < max_bool_arity; ++i) {
+                up.put<Bools>(bvars[i], bools::mkLit(bools::mk(BoolVar::next())));
+            }
+            up.put<Arith>(its->getLocVar(), arith::mkConst(c.rhs.loc));
+            const auto guard {ren(g) && theory::mkEq(its->getLocVar(), arith::mkConst(c.lhs.loc))};
+            its->addRule(Rule(guard, up), c.lhs.loc);
         }
-        bool_arg = 0;
-        int_arg = 0;
-        Subs up;
-        for (const auto &arg: c.rhs.args) {
-            std::visit(
-                Overload{[&](const Arith::Var var) {
-                             up.put<Arith>(vars[int_arg], ren.get<Arith>(var));
-                             ++int_arg;
-                         },
-                         [&](const Bools::Var var) {
-                             up.put<Bools>(bvars[bool_arg], ren.get<Bools>(var));
-                             ++bool_arg;
-                         }}, arg);
-        }
-        for (unsigned i = int_arg; i < max_int_arity; ++i) {
-            up.put<Arith>(vars[i], ArithVar::next()->toExpr());
-        }
-        for (unsigned i = bool_arg; i < max_bool_arity; ++i) {
-            up.put<Bools>(bvars[i], bools::mkLit(bools::mk(BoolVar::next())));
-        }
-        up.put<Arith>(its->getLocVar(), arith::mkConst(c.rhs.loc));
-        const auto guard {ren(c.guard) && theory::mkEq(its->getLocVar(), arith::mkConst(c.lhs.loc))};
-        its->addRule(Rule(guard, up), c.lhs.loc);
     }
     return its;
 }
