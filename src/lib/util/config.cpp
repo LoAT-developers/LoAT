261e23eba94917fe9bebf337d5a89a895a52db26
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2024-07-16 09:48:23 +0200

    enable recurrent exps

--- src/lib/util/config.cpp
+++ src/lib/util/config.cpp
@@ -110,7 +110,7 @@ namespace Config {
     namespace TIL {
         Mode mode {Forward};
         bool recurrent_cycles {false};
-        bool recurrent_exps {false};
+        bool recurrent_exps {true};
         bool recurrent_divs {true};
         bool recurrent_bounds {true};
         MbpKind mbpKind {IntMbp};
