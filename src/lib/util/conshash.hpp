782703d2b25abf3ec8e94279a7d7494b7224be01
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2024-09-30 16:17:49 +0200

    Revert "memory pool"
    This reverts commit a994a682c9014112c43a764c32253422d22158af.

--- src/lib/util/conshash.hpp
+++ src/lib/util/conshash.hpp
@@ -3,7 +3,6 @@
 #include "notnull.hpp"
 
 #include <unordered_map>
-#include <vector>
 #include <memory>
 
 template<class Abstract, class Concrete, class Hash, class Eq, class... Args>
@@ -19,19 +18,13 @@ private:
         Hash,
         Eq> cache{};
 
-    size_t capacity;
-    std::vector<Concrete*> pool;
-    Concrete* next;
-
 public:
 
-    ConsHash(const size_t capacity): cache(capacity), capacity(capacity) {
-        next = pool.emplace_back((Concrete*) malloc(capacity * sizeof(Concrete)));
-    }
+    ConsHash(const size_t capacity): cache(capacity) {}
 
     ~ConsHash() {
-        for (const auto &p: pool) {
-            free(p);
+        for (const auto &[_,v]: cache) {
+            delete v;
         }
     }
 
@@ -42,12 +35,8 @@ public:
     const Ptr from_cache(const Args&&... args) {
         const auto [it,b] {cache.emplace(std::make_tuple(args...), nullptr)};
         if (b) {
-            if (next == pool.back() + capacity) {
-                next = pool.emplace_back((Concrete*) malloc(capacity * sizeof(Concrete)));
-            }
-            const auto *res {new (next) Concrete(args...)};
+            const auto res {new Concrete(args...)};
             it->second = res;
-            ++next;
             return cpp::assume_not_null(res);
         }
         return cpp::assume_not_null(it->second);
@@ -56,12 +45,8 @@ public:
     const Ptr from_cache(const Args&... args) {
         const auto [it,b] {cache.emplace(std::make_tuple(args...), nullptr)};
         if (b) {
-            if (next == pool.back() + capacity) {
-                next = pool.emplace_back((Concrete*) malloc(capacity * sizeof(Concrete)));
-            }
-            const auto *res {new (next) Concrete(args...)};
+            const auto res {new Concrete(args...)};
             it->second = res;
-            ++next;
             return cpp::assume_not_null(res);
         }
         return cpp::assume_not_null(it->second);
