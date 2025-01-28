7b6b3d893d50a4838985f5c4f902f8c5be095664
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2025-01-28 15:42:49 +0100

    docu

--- src/lib/preprocessing/intmbp.cpp
+++ src/lib/preprocessing/intmbp.cpp
@@ -4,13 +4,20 @@
 
 Bools::Expr int_mbp(const Bools::Expr &t, const Model &model, const Arith::Var x, const Config::TRPConfig::MbpKind mode) {
     assert(t->isConjunction());
+    // divisibility constraints for x
     linked_hash_set<Divisibility> divs;
+    // lower bounds for x
     linked_hash_set<Arith::Expr> lb;
+    // upper bounds for x
     linked_hash_set<Arith::Expr> ub;
+    // least common multiple of all coefficients (or factors) of x in the given expression
     Int flcm{1};
+    // least common multiple of all divisors (or modulus) in divisibility constraints for x
     Int mlcm{1};
     auto lits{t->lits()};
     auto &arith_lits{lits.get<Arith::Lit>()};
+    // Iterate over all arithmetic literals to populate the sets above.
+    // Erases literals that contain x as side-effect.
     for (auto it = arith_lits.begin(); it != arith_lits.end();) {
         const auto &l{*it};
         if (l->has(x)) {
@@ -38,6 +45,7 @@ Bools::Expr int_mbp(const Bools::Expr &t, const Model &model, const Arith::Var x
                 if (l->isGt()) {
                     handle_gt(l->lhs());
                 } else if (l->isEq()) {
+                    // split equalities into two inequations
                     handle_gt(l->lhs() + arith::mkConst(1));
                     handle_gt(-l->lhs() + arith::mkConst(1));
                 }
@@ -67,6 +75,10 @@ Bools::Expr int_mbp(const Bools::Expr &t, const Model &model, const Arith::Var x
                     lit);
             });
     }
+    // In Cooper's QE procedure, we'd now sacale all literals so that the coefficient of x is flcm.
+    // We can't do that explicitly, as our expressions are normalized automatically.
+    // So instead, we collect all divisibility constraints / bounds for flcm * x in the sets below.
+    // Equivalently, assume that we substituted flcm * x with a fresh variable x', and the sets below contain the divisibility constraints / bounds for x'.
     linked_hash_set<Divisibility> scaled_divs;
     linked_hash_set<Arith::Expr> scaled_lb;
     linked_hash_set<Arith::Expr> scaled_ub;
@@ -86,12 +98,19 @@ Bools::Expr int_mbp(const Bools::Expr &t, const Model &model, const Arith::Var x
         const auto factor{flcm / d.factor};
         scaled_divs.insert(Divisibility{.factor = 1, .modulo = d.modulo * factor, .res = d.res * arith::mkConst(factor)});
     }
+    // in addition to the constraints above, we have flcm|x'
     scaled_divs.insert(Divisibility{.factor = 1, .modulo = flcm, .res = arith::mkConst(0)});
+    // compute the least common multiple of all divisors
     for (const auto &d : scaled_divs) {
         mlcm = mp::lcm(mlcm, d.modulo);
     }
+    // Compute the value that we substitute for x'.
+    // Here, we can either start from the closest upper bound, or from the closest lower bound.
+    // The decision depends on the given mode.
+    // If the mode is IntMbp, then we prefer upper over lower bounds iff there are fewer upper than lower bounds.
     Arith::Expr substitute {arith::mkConst(0)};
     if (mode == Config::TRPConfig::UpperIntMbp || (mode == Config::TRPConfig::IntMbp && scaled_ub.size() < scaled_lb.size())) {
+        // start from the closest upper bound
         auto closest_upper{*scaled_ub.begin()};
         auto min_val{closest_upper->eval(model.get<Arith>())};
         for (const auto &u : scaled_ub) {
@@ -101,15 +120,20 @@ Bools::Expr int_mbp(const Bools::Expr &t, const Model &model, const Arith::Var x
                 min_val = val;
                 break;
             }
+            // If two bounds have the same distance, we prefer non-constant over constant bounds, as they often lead to formulas that cover more cases.
+            // If both are (non-)constant, we simply compare the pointers to break the tie.
             if (val < min_val || (val == min_val && closest_upper->isRational() && !u->isRational()) || (val == min_val && u < closest_upper)) {
                 closest_upper = u;
                 min_val = val;
             }
         }
+        // compute i_l as in [Spacer], but negated (as we decided to pick an upper instead of a lower bound)
         const auto i_l{arith::mkMod(closest_upper - arith::mkConst(1) - arith::mkConst(flcm) * x, arith::mkConst(mlcm))};
+        // evaluate i_l in the current model
         const auto i_l_val{i_l->eval(model.get<Arith>())};
         substitute = closest_upper - arith::mkConst(1 + i_l_val);
     } else {
+        // start from the closest lower bound -- symmetric to the case above
         auto closest_lower{*scaled_lb.begin()};
         auto max_val{closest_lower->eval(model.get<Arith>())};
         for (const auto &l : scaled_lb) {
@@ -128,6 +152,7 @@ Bools::Expr int_mbp(const Bools::Expr &t, const Model &model, const Arith::Var x
         const auto i_l_val{i_l->eval(model.get<Arith>())};
         substitute = closest_lower + arith::mkConst(1 + i_l_val);
     }
+    // re-add the literals for x', but replace x' with the substitute computed above
     for (const auto &l : scaled_lb) {
         arith_lits.insert(arith::mkGt(substitute, l));
     }
