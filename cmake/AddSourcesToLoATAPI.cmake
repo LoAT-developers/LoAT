c0c7b87f160aee09cc93ff4fd6fead2a2e4b20bf
Moritz <mowe03@gmail.com>
2025-02-28 16:37:25 +0100

    Added static library that is linked to the executable
    TODO:
    - Comments
    - Notify user for missing (external) dependencies
    - (Fix include dirs)
    - (Fix interface links)

---
+++ cmake/AddSourcesToLoATAPI.cmake
@@ -0,0 +1,6 @@
+file(GLOB LOAT_API_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/*.cpp")
+if(LOAT_API_SOURCES)
+    target_sources(${Loat_API} PRIVATE ${LOAT_API_SOURCES})
+endif()
+target_include_directories(${Loat_API} PRIVATE $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>)
+target_include_directories(${EXECUTABLE} PRIVATE $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>)
