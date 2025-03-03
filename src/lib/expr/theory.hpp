5a76e61e56f8465da2410aa82b6a8ee2cc1a499d
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2025-03-03 16:33:29 +0100

    straightforward implementation of theory::apply

--- src/lib/expr/theory.hpp
+++ src/lib/expr/theory.hpp
@@ -84,8 +84,12 @@ ArrayType to_type(const Var &x);
 std::optional<Var> is_var(const Expr &x);
 
 template <class Int, class Bool, class ... Ts>
-auto apply(const std::variant<Int, Bool> &x, Ts... f) {
-    return std::visit(Overload{f...}, x);
+inline auto apply(const std::variant<Int, Bool> &x, Ts... f) noexcept {
+    if (std::holds_alternative<Int>(x)) {
+        return Overload{f...}(*std::get_if<Int>(&x));
+    } else {
+        return Overload{f...}(*std::get_if<Bool>(&x));
+    }
 }
 
 template <size_t I, ITheory T>
