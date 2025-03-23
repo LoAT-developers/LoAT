9940761e8c43cf9e9d5818750004f15adda6a286
Moritz <mowe03@gmail.com>
2025-03-23 12:54:21 +0100

    Update main.cpp

--- example/api-integration/src/main.cpp
+++ example/api-integration/src/main.cpp
@@ -10,12 +10,20 @@ int main()
         LoatConfig::InitialConfig::Mode::Complexity,
         LoatConfig::InitialConfig::SmtSolver::Z3);
 
+    LoatConfig::InitialConfig initialConfig2(
+        LoatConfig::InitialConfig::Format::Horn,
+        LoatConfig::InitialConfig::Engine::ADCL,
+        LoatConfig::InitialConfig::Mode::Complexity,
+        LoatConfig::InitialConfig::SmtSolver::Z3);
+
     LoatConfig::DynamicConfig dynamicConfig1;
     dynamicConfig1.direction = LoatConfig::DynamicConfig::Direction::Forward;
 
     LoatConfig config1(initialConfig1, dynamicConfig1);
+    LoatConfig config2(initialConfig1);
 
     LoatSolver solver1(config1);
+    LoatSolver solver2(config2);
 
     solver1.setParameter(DynamicParameterKey::Log, true);
     solver1.refreshConfig();
