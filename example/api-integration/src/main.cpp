cac027610d8b0a030909cdaa0fbba66b357519c8
Moritz <mowe03@gmail.com>
2025-04-08 12:31:40 +0000

    Add sample to api Integration

--- example/api-integration/src/main.cpp
+++ example/api-integration/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "loatconfig.hpp"
 #include "loatsolver.hpp"
+#include "loatexpression.hpp"
 #include <any>
 #include <stdexcept>
 
@@ -15,64 +16,23 @@ int main()
         LoatConfig::InitialConfig::MbpKind::IntMbp,
         false);
 
-    LoatConfig::InitialConfig initialConfig2(
-        LoatConfig::InitialConfig::Format::Horn,
-        LoatConfig::InitialConfig::Engine::ADCL,
-        LoatConfig::InitialConfig::Mode::Complexity,
-        LoatConfig::InitialConfig::SmtSolver::Z3,
-        LoatConfig::InitialConfig::Direction::Forward,
-        LoatConfig::InitialConfig::MbpKind::IntMbp,
-        false);
-
-    LoatConfig::DynamicConfig dynamicConfig1;
-    dynamicConfig1.set(DynamicParameterKey::Log, false);
-
-    try
-    {
-        dynamicConfig1.set(DynamicParameterKey::Log, 12);
-    }
-    catch (const std::invalid_argument &e)
-    {
-        std::cout << "Caught expected: " << e.what() << std::endl;
-    }
-
-    LoatConfig config1(initialConfig1, dynamicConfig1);
-    LoatConfig config2(initialConfig1);
-
-    dynamicConfig1.set(DynamicParameterKey::Log, true);
-
-    try
-    {
-        dynamicConfig1.set(DynamicParameterKey::Log, 12);
-    }
-    catch (const std::invalid_argument &e)
-    {
-        std::cout << "Caught expected:" << e.what() << std::endl;
-    }
-
-    LoatConfig config3(LoatConfig::InitialConfig(LoatConfig::InitialConfig::Format::Koat,
-                                                 LoatConfig::InitialConfig::Engine::ADCL,
-                                                 LoatConfig::InitialConfig::Mode::Complexity,
-                                                 LoatConfig::InitialConfig::SmtSolver::Z3,
-                                                 LoatConfig::InitialConfig::Direction::Forward,
-                                                 LoatConfig::InitialConfig::MbpKind::IntMbp,
-                                                 false));
+    LoatConfig config1(initialConfig1);
 
     LoatSolver solver1(config1);
-    LoatSolver solver2(config2);
-    LoatSolver solver3(config3);
 
     solver1.setParameter(DynamicParameterKey::Log, true);
-    try
-    {
-        solver1.setParameter(DynamicParameterKey::Log, 12);
-    }
-    catch (const std::invalid_argument &e)
-    {
-        std::cout << "Caught expected: " << e.what() << std::endl;
-    }
-
     solver1.refreshConfig();
+
+    LoatExprPtr five = LoatExpression::mkConst(Rational(5));
+    LoatExprPtr x = LoatExpression::mkVar(1);
+    LoatExprPtr ten = LoatExpression::mkConst(Rational(10));
+
+    LoatExprPtr fivex = LoatExpression::mkTimes({five, x});
+    LoatExprPtr fivexplusten = LoatExpression::mkPlus(fivex, ten);
+
+    std::cout << fivexplusten << std::endl;
+
+    solver1.add(fivexplusten);
 
     return 0;
 }
