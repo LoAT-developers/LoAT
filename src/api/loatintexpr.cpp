fb2a5a7d8bf1f0efebc74302c8e1ac5ee27201f0
Moritz <mowe03@gmail.com>
2025-04-15 14:08:33 +0000

    Merge all intexpr types into one implementation file to reduce confusion

--- src/api/loatintexpr.cpp
+++ src/api/loatintexpr.cpp
@@ -1,9 +1,13 @@
 #include "loatintexpr.hpp"
 #include "optional.hpp"
 #include "linkedhashmap.hpp"
+#include "vector.hpp"
 
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/functional/hash.hpp>
 #include <numeric>
 #include <sstream>
+#include <string>
 
 // Hash function for expression pointers
 std::size_t hash_value(const LoatIntExprPtr &x)
@@ -130,4 +134,245 @@ std::ostream &operator<<(std::ostream &s, const LoatIntExprPtr e)
     default:
         return s << "<unknown expr>";
     }
+}
+
+// Add
+ConsHash<LoatIntExpr, LoatIntAdd, LoatIntAdd::CacheHash, LoatIntAdd::CacheEqual, LoatIntExprSet> LoatIntAdd::cache;
+
+LoatIntAdd::LoatIntAdd(const LoatIntExprSet &args) : LoatIntExpr(LoatIntExpression::Kind::Plus), m_args(args) {}
+
+LoatIntAdd::~LoatIntAdd()
+{
+    cache.erase(m_args);
+}
+
+bool LoatIntAdd::CacheEqual::operator()(const std::tuple<LoatIntExprSet> &args1, const std::tuple<LoatIntExprSet> &args2) const noexcept
+{
+    return args1 == args2;
+}
+
+size_t LoatIntAdd::CacheHash::operator()(const std::tuple<LoatIntExprSet> &args) const noexcept
+{
+    size_t hash{42};
+    const auto &children{std::get<0>(args)};
+    boost::hash_combine(hash, boost::hash_unordered_range(children.begin(), children.end()));
+    return hash;
+}
+
+LoatIntExprPtr LoatIntExpression::mkPlus(LoatIntExprPtr fst, LoatIntExprPtr snd)
+{
+    return mkPlus({fst, snd});
+}
+
+LoatIntExprPtr LoatIntExpression::mkPlus(std::vector<LoatIntExprPtr> &&args)
+{
+    if (args.empty())
+    {
+        return mkConst(0);
+    }
+    if (args.size() == 1)
+    {
+        return args[0];
+    }
+    const LoatIntExprSet arg_set{args.begin(), args.end()};
+    return LoatIntAdd::cache.from_cache(std::move(arg_set));
+}
+
+const LoatIntExprSet &LoatIntAdd::getArgs() const
+{
+    return m_args;
+}
+
+// Const
+ConsHash<LoatIntExpr, LoatIntConst, LoatIntConst::CacheHash, LoatIntConst::CacheEqual, Rational> LoatIntConst::cache;
+
+LoatIntConst::LoatIntConst(const Rational &t) : LoatIntExpr(LoatIntExpression::Kind::Constant), m_value(t) {}
+
+LoatIntConst::~LoatIntConst()
+{
+    cache.erase(m_value);
+}
+
+const Rational &LoatIntConst::operator*() const
+{
+    return m_value;
+}
+
+const Rational &LoatIntConst::getValue() const
+{
+    return m_value;
+}
+
+bool LoatIntConst::CacheEqual::operator()(const std::tuple<Rational> &args1, const std::tuple<Rational> &args2) const noexcept
+{
+    return std::get<0>(args1) == std::get<0>(args2);
+}
+
+size_t LoatIntConst::CacheHash::operator()(const std::tuple<Rational> &args) const noexcept
+{
+    return std::hash<Rational>{}(std::get<0>(args));
+}
+
+LoatIntExprPtr LoatIntExpression::mkConst(const Rational &r)
+{
+    return LoatIntConst::cache.from_cache(r);
+}
+
+LoatIntExprPtr LoatIntExpression::mkConst(const Rational &&r)
+{
+    return mkConst(r);
+}
+
+// Exp
+ConsHash<LoatIntExpr, LoatIntExp, LoatIntExp::CacheHash, LoatIntExp::CacheEqual, LoatIntExprPtr, LoatIntExprPtr> LoatIntExp::cache;
+
+LoatIntExp::LoatIntExp(const LoatIntExprPtr base, const LoatIntExprPtr exponent)
+    : LoatIntExpr(LoatIntExpression::Kind::Exp), m_base(base), m_exponent(exponent) {}
+
+LoatIntExp::~LoatIntExp()
+{
+    cache.erase(m_base, m_exponent);
+}
+
+LoatIntExprPtr LoatIntExp::getBase() const
+{
+    return m_base;
+}
+
+LoatIntExprPtr LoatIntExp::getExponent() const
+{
+    return m_exponent;
+}
+
+bool LoatIntExp::CacheEqual::operator()(const std::tuple<LoatIntExprPtr, LoatIntExprPtr> &a,
+                                        const std::tuple<LoatIntExprPtr, LoatIntExprPtr> &b) const noexcept
+{
+    return a == b;
+}
+
+size_t LoatIntExp::CacheHash::operator()(const std::tuple<LoatIntExprPtr, LoatIntExprPtr> &a) const noexcept
+{
+    size_t hash = {0};
+    boost::hash_combine(hash, std::get<0>(a));
+    boost::hash_combine(hash, std::get<1>(a));
+    return hash;
+}
+
+LoatIntExprPtr LoatIntExpression::mkExp(const LoatIntExprPtr base, const LoatIntExprPtr exponent)
+{
+    return LoatIntExp::cache.from_cache(base, exponent)->toPtr();
+}
+
+// Mod
+ConsHash<LoatIntExpr, LoatIntMod, LoatIntMod::CacheHash, LoatIntMod::CacheEqual, LoatIntExprPtr, LoatIntExprPtr> LoatIntMod::cache;
+
+LoatIntMod::LoatIntMod(const LoatIntExprPtr lhs, const LoatIntExprPtr rhs)
+    : LoatIntExpr(LoatIntExpression::Kind::Mod), m_lhs(lhs), m_rhs(rhs) {}
+
+LoatIntMod::~LoatIntMod()
+{
+    cache.erase(m_lhs, m_rhs);
+}
+
+const LoatIntExprPtr LoatIntMod::getLhs() const
+{
+    return m_lhs;
+}
+
+const LoatIntExprPtr LoatIntMod::getRhs() const
+{
+    return m_rhs;
+}
+
+bool LoatIntMod::CacheEqual::operator()(const std::tuple<LoatIntExprPtr, LoatIntExprPtr> &args1,
+                                        const std::tuple<LoatIntExprPtr, LoatIntExprPtr> &args2) const noexcept
+{
+    return args1 == args2;
+}
+
+size_t LoatIntMod::CacheHash::operator()(const std::tuple<LoatIntExprPtr, LoatIntExprPtr> &args) const noexcept
+{
+    size_t hash = 23;
+    const auto &lhs = std::get<0>(args);
+    const auto &rhs = std::get<1>(args);
+    boost::hash_combine(hash, lhs);
+    boost::hash_combine(hash, rhs);
+    return hash;
+}
+
+LoatIntExprPtr LoatIntExpression::mkMod(LoatIntExprPtr lhs, LoatIntExprPtr rhs)
+{
+    return LoatIntMod::cache.from_cache(lhs, rhs)->toPtr();
+}
+
+// Mult
+ConsHash<LoatIntExpr, LoatIntMult, LoatIntMult::CacheHash, LoatIntMult::CacheEqual, LoatIntExprSet> LoatIntMult::cache;
+
+LoatIntMult::LoatIntMult(const LoatIntExprSet &args) : LoatIntExpr(LoatIntExpression::Kind::Times), m_args(args) {}
+
+LoatIntMult::~LoatIntMult()
+{
+    cache.erase(m_args);
+}
+
+const LoatIntExprSet &LoatIntMult::getArgs() const
+{
+    return m_args;
+}
+
+bool LoatIntMult::CacheEqual::operator()(const std::tuple<LoatIntExprSet> &a, const std::tuple<LoatIntExprSet> &b) const noexcept
+{
+    return a == b;
+}
+
+size_t LoatIntMult::CacheHash::operator()(const std::tuple<LoatIntExprSet> &a) const noexcept
+{
+    size_t hash = {23};
+    const auto &children{std::get<0>(a)};
+    boost::hash_combine(hash, boost::hash_unordered_range(children.begin(), children.end()));
+    return hash;
+}
+
+LoatIntExprPtr LoatIntExpression::mkTimes(LoatIntExprVec &&args)
+{
+    if (args.empty())
+        return mkConst(1);
+    if (args.size() == 1)
+        return args[0];
+    return LoatIntMult::cache.from_cache(LoatIntExprSet(args.begin(), args.end()))->toPtr();
+}
+
+LoatIntExprPtr LoatIntExpression::mkTimes(const LoatIntExprPtr a, const LoatIntExprPtr b)
+{
+    return mkTimes(LoatIntExprVec{a, b});
+}
+
+// Var
+ConsHash<LoatIntExpr, LoatIntVar, LoatIntVar::CacheHash, LoatIntVar::CacheEqual, std::string> LoatIntVar::cache;
+
+LoatIntVar::LoatIntVar(const std::string &name) : LoatIntExpr(LoatIntExpression::Kind::Variable), m_name(name) {}
+
+LoatIntVar::~LoatIntVar()
+{
+    cache.erase(m_name);
+}
+
+std::string LoatIntVar::getName() const
+{
+    return m_name;
+}
+
+bool LoatIntVar::CacheEqual::operator()(const std::tuple<std::string> &a, const std::tuple<std::string> &b) const noexcept
+{
+    return a == b;
+}
+
+size_t LoatIntVar::CacheHash::operator()(const std::tuple<std::string> &a) const noexcept
+{
+    return std::hash<std::string>{}(std::get<0>(a));
+}
+
+LoatIntExprPtr LoatIntExpression::mkVar(const std::string &name)
+{
+    return LoatIntVar::cache.from_cache(name)->toPtr();
 }
