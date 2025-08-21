dbda42a4239882979a1f972498ad5299d398c2cb
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2025-08-21 13:19:52 +0200

    ditch old formats and antlr

--- grammars/test_grammar.sh
+++
@@ -1,4 +0,0 @@
-# usage: test_grammar.sh $GRAMMAR_NAME $START_RULE $TEST_FILE
-javac -cp ../deps/antlr-4.11.1-complete.jar:$CLASSPATH *.java
-java -cp ../deps/antlr-4.11.1-complete.jar:$CLASSPATH org.antlr.v4.gui.TestRig $@
-
