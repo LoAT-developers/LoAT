fade2b584fb859e22554fce8fc8299e46ed485a1
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2025-08-29 14:19:18 +0200

    chc parser: fix bug wrt let expressions

--- src/parser/util/smtlibutil.cpp
+++ src/parser/util/smtlibutil.cpp
@@ -48,6 +48,12 @@ Arith::Expr parseArithExpr(sexpresso::Sexp &exp, SMTLibParsingState &state) {
         if (isInt(name)) {
             return arith::mkConst(Int(name));
         } else {
+            for (int i = state.bindings.size() - 1; i >= 0; i--) {
+                const auto it{state.bindings[i].find(name)};
+                if (it != state.bindings[i].end()) {
+                    return std::get<Arith::Expr>(it->second);
+                }
+            }
             return std::get<Arith::Var>(state.get_var(name, theory::Type::Int));
         }
     } else {
