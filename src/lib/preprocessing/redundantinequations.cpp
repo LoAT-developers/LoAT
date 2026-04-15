92574b46a0406b1a9b2c02b59226a6572e05f44f
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2026-04-15 10:28:30 +0200

    redundant inequations: cache cells

--- src/lib/preprocessing/redundantinequations.cpp
+++ src/lib/preprocessing/redundantinequations.cpp
@@ -8,16 +8,17 @@ Bools::Expr removeRedundantInequations(const Bools::Expr& e) {
     // collect linear bounds of the form b >= 0
     auto lits {e->lits()};
     auto &arith_lits {lits.get<Arith::Lit>()};
-    ArithExprVec bounded;
+    linked_hash_map<Arith::Expr, linked_hash_set<ArithVarPtr>> bounded;
     for (auto it = arith_lits.begin(); it != arith_lits.end();) {
         const auto lit {*it};
         if (const auto lhs {lit->lhs()}; lhs->isLinear()) {
             if (lit->isGt()) {
-                bounded.push_back(lhs - arith::one());
+                bounded.emplace(lhs - arith::one(), lhs->cells());
                 it = arith_lits.erase(it);
             } else if (lit->isEq()) {
-                bounded.push_back(lhs);
-                bounded.push_back(-lhs);
+                const auto cells = lhs->cells();
+                bounded.emplace(lhs, cells);
+                bounded.emplace(-lhs, cells);
                 it = arith_lits.erase(it);
             } else {
                 ++it;
@@ -28,10 +29,10 @@ Bools::Expr removeRedundantInequations(const Bools::Expr& e) {
     }
     // set up one non-negative multiplier for each bound
     auto solver {SmtFactory::modelBuildingSolver(Logic::QF_LA)};
-    std::vector<ArithVarPtr> factors;
-    for (size_t i = 0; i < bounded.size(); ++i) {
+    std::unordered_map<Arith::Expr, ArithVarPtr> factors;
+    for (const auto& b: bounded | std::views::keys) {
         const auto f {arrays::nextConst<Arith>()};
-        factors.push_back(f);
+        factors.emplace(b, f);
         solver->add(arith::mkGeq(f, arith::zero()));
     }
     const auto arith_vars {e->cells().get<ArithVarPtr>()};
@@ -40,25 +41,25 @@ Bools::Expr removeRedundantInequations(const Bools::Expr& e) {
         // set up one equation for each variable
         for (const auto &x: arith_vars) {
             ArithExprVec addends;
-            for (unsigned idx = 0; idx < bounded.size(); ++idx) {
-                if (*it != bounded[idx] && bounded[idx]->has(x)) {
-                    addends.push_back(*bounded[idx]->coeff(x) * factors[idx]);
+            for (const auto& [b, cells]: bounded) {
+                if (it->first != b && cells.contains(x)) {
+                    addends.push_back(*b->coeff(x) * factors.at(b));
                 }
             }
-            solver->add(arith::mkEq(*(*it)->coeff(x), arith::mkPlus(std::move(addends))));
+            solver->add(arith::mkEq(*it->first->coeff(x), arith::mkPlus(std::move(addends))));
         }
         // and one equation for the constant part
         ArithExprVec addends;
-        for (unsigned idx = 0; idx < bounded.size(); ++idx) {
-            if (*it != bounded[idx]) {
-                addends.push_back(arith::mkConst(bounded[idx]->getConstantAddend()) * factors[idx]);
+        for (const auto& b: bounded | std::views::keys) {
+            if (it->first != b) {
+                addends.push_back(arith::mkConst(b->getConstantAddend()) * factors.at(b));
             }
         }
-        solver->add(arith::mkGeq(arith::mkConst((*it)->getConstantAddend()), arith::mkPlus(std::move(addends))));
+        solver->add(arith::mkGeq(arith::mkConst(it->first->getConstantAddend()), arith::mkPlus(std::move(addends))));
         if (solver->check() == SmtResult::Sat) {
             it = bounded.erase(it);
         } else {
-            arith_lits.insert(arith::mkGeq(*it, arith::zero()));
+            arith_lits.insert(arith::mkGeq(it->first, arith::zero()));
             ++it;
         }
         solver->pop();
