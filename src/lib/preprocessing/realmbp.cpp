fbcdd166e5df3299cd117b3e50296bc43b895ea2
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2026-04-15 15:09:09 +0200

    revert realmbp for testing

--- src/lib/preprocessing/realmbp.cpp
+++ src/lib/preprocessing/realmbp.cpp
@@ -2,30 +2,28 @@
 #include "mbputil.hpp"
 #include "smtfactory.hpp"
 
-Bools::Expr real_mbp(const Bools::Expr &t, const ModelPtr &model, const ArithVarPtr& x, Subs& subs) {
+Bools::Expr real_mbp(const Bools::Expr &t, const ModelPtr &model, const ArithVarPtr& x) {
     auto bounds {t->getBounds(x)};
     const auto it {std::ranges::find_if(bounds, [](const auto &b) {
         return b.kind == BoundKind::Equality;
     })};
     if (it != bounds.end()) {
-        subs = subs.compose(Subs::build(x->var(), arrays::update(x, it->bound)));
-        return t;
+        return t->subs(Subs::build(x->var(), arrays::update(x, it->bound)));
     }
     if (const auto closest {mbp::closest_lower_bound(bounds, model, x)}) {
-        subs = subs.compose(Subs::build(x->var(), arrays::update(x, *closest)));
-        return t;
+        return t->subs(Subs::build(x->var(), arrays::update(x, *closest)));
     }
     return t->toMinusInfinity(x);
 }
 
-Bools::Expr real_mbp(const Bools::Expr& t, const ModelPtr& model, const Cell& x, Subs& subs) {
+Bools::Expr real_mbp(const Bools::Expr& t, const ModelPtr& model, const Cell& x) {
     return theory::apply(
         x,
         [&](const Bools::Var& x) {
             return mbp::bool_mbp(t, model, x);
         },
         [&](const ArithVarPtr& x) {
-            const auto res{real_mbp(t, model, x, subs)};
+            const auto res{real_mbp(t, model, x)};
             assert(res != bot());
             return res;
         });
@@ -33,14 +31,11 @@ Bools::Expr real_mbp(const Bools::Expr& t, const ModelPtr& model, const Cell& x,
 
 Bools::Expr mbp::real_mbp(const Bools::Expr &trans, const ModelPtr &model, const std::function<bool(const Cell&)> &eliminate) {
     Bools::Expr res {trans};
-    Subs subs;
     for (const auto &x: trans->cells()) {
         if (eliminate(x)) {
-            res = ::real_mbp(res, model, x, subs);
+            res = ::real_mbp(res, model, x);
         }
     }
-    res = res->subs(subs);
-    assert(res != bot());
     return res;
 }
 
