cf9f5879bed0f9e34056160d64a51dcf6f118a97
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2026-04-22 16:12:02 +0200

    Revert "adcl_sat: use mbp for trace"
    This reverts commit 5c1ab44c3c194d141d9a6c011bf13ba40d407c1a.

--- src/adclsat/adclsat.cpp
+++ src/adclsat/adclsat.cpp
@@ -268,7 +268,7 @@ std::optional<SmtResult> ADCLSat::do_step() {
     const auto trans{rule_map.at(id)};
     const auto m{(*model)->composeBackwards(subs)};
     const auto [imp_non_bool, imp_bool] = m->structuralImplicant(trans);
-    const auto imp = trp.mbp(imp_non_bool && imp_bool, m, theory::isTempCell);
+    const auto imp = imp_non_bool && imp_bool;
     solver->add(imp->renameVars(subs));
     const auto smt_res{solver->check()};
     assert(smt_res == SmtResult::Sat);
